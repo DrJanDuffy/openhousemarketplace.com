fa46e9f3dc1c6431fffe4950243b5018bed0a8a7
genekellyboyle <DrDuffy@bhhsnv.com>
2025-11-01 06:45:29 -0700

    Fix Google Analytics detection by using Next.js Script component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "styles/tailwind.css"
 import { Metadata } from "next"
+import Script from "next/script"
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://www.openhousemarketplace.com'),
@@ -27,23 +28,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-G22H2J3EMX" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-G22H2J3EMX');
-            `,
-          }}
-        />
-        <script
-          src="https://em.realscout.com/widgets/realscout-web-components.umd.js"
-          type="module"
-          async
-        />
         <style
           dangerouslySetInnerHTML={{
             __html: `
@@ -83,7 +67,31 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           }}
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-G22H2J3EMX"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-G22H2J3EMX');
+            `,
+          }}
+        />
+        <Script
+          src="https://em.realscout.com/widgets/realscout-web-components.umd.js"
+          type="module"
+          strategy="afterInteractive"
+        />
+        {children}
+      </body>
     </html>
   )
 }
